5326708e7d95bbd1d2266ed627526519d8e8bfee
Your Name <you@example.com>
2024-09-27 23:54:50 +0530

    added all files

---
+++ task.js
@@ -0,0 +1,93 @@
+//snippet
+var arr=[1,2,3,{ name:"sruthi",age:21 },30];
+console.log(arr.length);
+//1st
+var arr1=[1,2,3,4];
+arr1.push(40)
+console.log(arr1);
+//2nd
+var array=["radha","krishna","ayan"];
+array.pop()
+console.log(array);
+//3rd
+var arr=["boat","zebronics"];
+arr.unshift("oneplus");
+console.log(arr);
+//4th
+var arr=["moto","redmi","apple"];
+arr.shift();
+console.log(arr);
+//5th
+var system=["dell","hp","assus"];
+system.reverse()
+console.log(system);
+//6th
+var fruits=["apple","beetroot","grapes"];
+var index=fruits.indexOf("beetroot");
+console.log(index);
+//7th
+var colors=["red","blue",5, "black","purple"];
+var inc=colors.includes(5)
+console.log(inc);
+//8th
+var numbers=[5,6,9,1,3,0];
+numbers.sort()
+console.log(numbers);
+//9th
+var string=["hi","how","are","you"];
+var convert=string.join(" ")
+console.log(convert);
+//multiple
+//1st
+var num=[1,2,3,4]
+var addend=num.push(6)
+console.log(num);
+num.shift()
+console.log(num);
+//2nd
+var array2=["good","morning","fellow"]
+array2.reverse()
+console.log(array2);
+var join=array2.join("")
+console.log(join);
+//3rd
+var phone=[3,8,5,9]
+phone.sort()
+console.log(phone);
+phone.pop()
+console.log(phone);
+//4th
+var h=["NTR","RC","VD"];
+h.unshift("Ram","AA")
+console.log(h);
+h.shift()
+console.log(h);
+console.log(h.length);
+//5th
+var d1=[2,6,1];
+var d2=[3,5,7];
+var r=d1.concat(d2);
+console.log(r);
+var o=r.sort();
+console.log(o);
+o.pop()
+console.log(o);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
